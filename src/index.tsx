4ad60628e0299b7036e62a6e2f6f9dfd7d56291c
Mark <glmarkgl4@gmail.com>
2021-02-03 22:18:48 +0300

    Added antd css.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from 'app/App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
